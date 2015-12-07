1255edccdde44c37f35ea8c70c66cf0b973aea8e
James Gillmore <james@faceyspacey.com>
2015-12-07 11:12:05 -0800

    readme underscore mixin

--- package.js
+++ package.js
@@ -8,14 +8,16 @@ Package.describe({
 
 Package.onUse(function (api) {
 	api.versionsFrom('METEOR@1.2.1');
-	api.use('underscore');
+	
 	api.use('ecmascript@0.1.5');
 	
+	api.use('underscore');
+	api.use('ultimatejs:underscore-mixin@0.0.1');
 	
 	api.addFiles([
 		'event-selector-mixin.js',
-		'underscore-mixin.js',
 	]);
+	
 	api.export('EventSelector');
 });
 
